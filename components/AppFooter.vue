b64301a8f5774673f649bd262851f25d57ff042c
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-09-02 00:59:18 +0200

    Finished footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,18 +1,20 @@
 <template>
-  <footer class="flex flex-row space-x-4 text-sm justify-center">
-    <div class="text-white text-">
-      © 2023 Jakub Sobieraj. All rights reserved.
-    </div>
-    <div class="flex space-x-2">
-      <a href="https://github.com/qbibubi/"
-        ><img
-          src="/img/github-mark-white.png"
-          alt="GitHub profile"
-          class="w-7 h-7"
-      /></a>
-      <a href="https://www.linkedin.com/in/jakub-sobieraj-11b96025a/"
-        ><img src="/img/In-White-14@2x.png" alt="LinkedIn profile"
-      /></a>
+  <footer class="absolute bottom-0 inset-x-0 bg-transparent">
+    <div class="flex justify-center text-center items-center space-x-4 p-2">
+      <div class="text-md text-white text-middle">
+        © 2023 Jakub Sobieraj. All rights reserved.
+      </div>
+      <div class="flex flex-row space-x-2">
+        <a href="https://github.com/qbibubi/"
+          ><img
+            src="/img/github-mark-white.png"
+            alt="GitHub profile"
+            class="w-7 h-7"
+        /></a>
+        <a href="https://www.linkedin.com/in/jakub-sobieraj-11b96025a/"
+          ><img src="/img/In-White-14@2x.png" alt="LinkedIn profile"
+        /></a>
+      </div>
     </div>
   </footer>
 </template>
