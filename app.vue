382c00a9bf253c571d59a55fc87497069ad732d6
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-09-02 16:04:59 +0200

    update main

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex text-color-white w-screen h-screen m-0 bg-gray-900">
-    <div class="container mx-auto flex-col justify-center bg-gray-800">
-      <AppHeader class="bg-gray-700" />
-      <NuxtPage class="bg-gray-800 text-white" />
-      <AppFooter class="bg-gray-700" />
+  <div class="flex text-white w-screen h-screen m-0 bg-gray-900 font-mono">
+    <div class="container mx-auto min-w-100 justify-center bg-gray-800">
+      <AppHeader />
+      <NuxtPage class="bg-gray-800 text-left prose p-4" />
+      <AppFooter />
     </div>
   </div>
 </template>
