60015d25061f6ef87a45bd1bf594297993a36cdf
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-10-23 09:57:42 +0200

    Remove overengineered files

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex text-white w-screen h-screen m-0 bg-gray-900 font-mono">
-    <div class="container mx-auto min-w-100 justify-center bg-gray-800">
-      <AppHeader />
-      <NuxtPage class="bg-gray-800 text-left prose p-4" />
-      <AppFooter />
-    </div>
-  </div>
-</template>
